7ab0d4af697b2c0af60721a49e74ea3ad5ccf792
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-09-09 08:37:45 -0400

    For now use main to validate functionality still works

--- app/src/main/java/com/vandenbreemen/modernsimmingapp/MainActivity.kt
+++ app/src/main/java/com/vandenbreemen/modernsimmingapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.vandenbreemen.modernsimmingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.vandenbreemen.modernsimmingapp.data.googlegroups.GooglePostContentLoader
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun testLoadingPost(view: View) {
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val content = GooglePostContentLoader().getPostBody("https://groups.google.com/d/msg/uss-odyssey-oe/jLebMNq2V9U/t3XY7i1FAgAJ")
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, content, Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }

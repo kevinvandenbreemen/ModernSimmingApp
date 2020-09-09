ea010f3f1e74122b162ca5b328cfeb2a4bdc8238
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-09-08 22:45:43 -0400

    Initial commit

---
+++ app/src/main/java/com/vandenbreemen/modernsimmingapp/data/googlegroups/GoogleGroupsAPI.kt
@@ -0,0 +1,14 @@
+package com.vandenbreemen.modernsimmingapp.data.googlegroups
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface GoogleGroupsAPI {
+    @GET("forum/feed/{groupName}/msgs/rss.xml")
+    fun getRssFeed(
+        @Path("groupName")
+        groupName: String,
+        @Query("num")
+        postCount: Int): GoogleGroupsRSSFeed
+}
---
+++ app/src/main/java/com/vandenbreemen/modernsimmingapp/data/googlegroups/GoogleGroupsPost.kt
@@ -0,0 +1,25 @@
+package com.vandenbreemen.modernsimmingapp.data.googlegroups
+
+import org.simpleframework.xml.Element
+import org.simpleframework.xml.Root
+
+@Root(name = "item", strict = false)
+data class GoogleGroupsPost(
+
+    @set:Element(name = "title")
+    @get:Element(name = "title")
+    var title: String? = null,
+
+    @set:Element(name = "link")
+    @get:Element(name = "link")
+    var link: String? = null,
+
+    @set:Element(name = "author")
+    @get:Element(name = "author")
+    var author:String? = null,
+
+    @get:Element
+    @set:Element
+    var pubDate: String? = null
+
+) {}

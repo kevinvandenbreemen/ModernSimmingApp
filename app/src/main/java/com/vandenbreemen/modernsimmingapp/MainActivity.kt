21be76e7fd884d9d1f43f28a0b019ad42baec5ea
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-09-09 09:05:28 -0400

    Simple validation that loading post feed works

--- app/src/main/java/com/vandenbreemen/modernsimmingapp/MainActivity.kt
+++ app/src/main/java/com/vandenbreemen/modernsimmingapp/MainActivity.kt
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.vandenbreemen.modernsimmingapp.data.googlegroups.GoogleGroupsAPI
 import com.vandenbreemen.modernsimmingapp.data.googlegroups.GooglePostContentLoader
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory
+
+const val GOOGLE_GROUPS_BASE_URL = "https://groups.google.com/"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,5 +32,23 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    fun testLoadingGroup(view: View) {
+        CoroutineScope(Dispatchers.IO).launch {
+
+            val googleGroupsApi = Retrofit.Builder().baseUrl(GOOGLE_GROUPS_BASE_URL)
+                .addConverterFactory(SimpleXmlConverterFactory.create()).build().create(
+                    GoogleGroupsAPI::class.java)
+
+
+            val list = googleGroupsApi.getRssFeed("uss-odyssey-oe", 5).execute().body()?.articleList?.map { art->art.title }
+            list?.let { posts->
+                withContext(Main) {
+                    Toast.makeText(this@MainActivity, posts.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+
+        }
     }
 }
--- app/src/main/java/com/vandenbreemen/modernsimmingapp/data/googlegroups/GoogleGroupsAPI.kt
+++ app/src/main/java/com/vandenbreemen/modernsimmingapp/data/googlegroups/GoogleGroupsAPI.kt
@@ -1,5 +1,6 @@
 package com.vandenbreemen.modernsimmingapp.data.googlegroups
 
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -10,5 +11,5 @@ interface GoogleGroupsAPI {
         @Path("groupName")
         groupName: String,
         @Query("num")
-        postCount: Int): GoogleGroupsRSSFeed
+        postCount: Int): Call<GoogleGroupsRSSFeed>
 }

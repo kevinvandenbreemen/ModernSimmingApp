e0652d8d9407cb40912664b25bc94c862d7a6cec
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-09-09 19:06:46 -0400

    Remove redundant suspend keyword

--- app/src/main/java/com/vandenbreemen/modernsimmingapp/data/repository/GoogleGroupsRepository.kt
+++ app/src/main/java/com/vandenbreemen/modernsimmingapp/data/repository/GoogleGroupsRepository.kt
@@ -8,7 +8,7 @@ import com.vandenbreemen.modernsimmingapp.data.googlegroups.GoogleGroupsPost
  */
 class GoogleGroupsRepository(private val api: GoogleGroupsAPI) {
 
-    suspend fun getSims(groupName: String, num: Int): List<GoogleGroupsPost>? {
+    fun getSims(groupName: String, num: Int): List<GoogleGroupsPost>? {
         val call = api.getRssFeed(groupName, num)
         val response = call.execute()
         if(response.isSuccessful) {

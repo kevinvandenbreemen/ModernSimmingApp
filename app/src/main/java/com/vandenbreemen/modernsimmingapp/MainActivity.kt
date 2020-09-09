12dbb20f7b4d25742814716cff59274cbb396676
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-09-09 19:02:22 -0400

    Move fetching posts to repository

--- app/src/main/java/com/vandenbreemen/modernsimmingapp/MainActivity.kt
+++ app/src/main/java/com/vandenbreemen/modernsimmingapp/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.vandenbreemen.modernsimmingapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.vandenbreemen.modernsimmingapp.data.googlegroups.GoogleGroupsAPI
 import com.vandenbreemen.modernsimmingapp.data.googlegroups.GooglePostContentLoader
+import com.vandenbreemen.modernsimmingapp.data.repository.GoogleGroupsRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -18,9 +18,18 @@ import retrofit2.converter.simplexml.SimpleXmlConverterFactory
 const val GOOGLE_GROUPS_BASE_URL = "https://groups.google.com/"
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var googleGroupsRepository: GoogleGroupsRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val googleGroupsApi = Retrofit.Builder().baseUrl(GOOGLE_GROUPS_BASE_URL)
+            .addConverterFactory(SimpleXmlConverterFactory.create())
+            .build().create(
+                GoogleGroupsAPI::class.java)
+        googleGroupsRepository = GoogleGroupsRepository(googleGroupsApi)
     }
 
     fun testLoadingPost(view: View) {
@@ -37,18 +46,15 @@ class MainActivity : AppCompatActivity() {
     fun testLoadingGroup(view: View) {
         CoroutineScope(Dispatchers.IO).launch {
 
-            val googleGroupsApi = Retrofit.Builder().baseUrl(GOOGLE_GROUPS_BASE_URL)
-                .addConverterFactory(SimpleXmlConverterFactory.create()).build().create(
-                    GoogleGroupsAPI::class.java)
-
-
-            val list = googleGroupsApi.getRssFeed("uss-odyssey-oe", 5).execute().body()?.articleList?.map { art->art.title }
-            list?.let { posts->
-                withContext(Main) {
-                    Toast.makeText(this@MainActivity, posts.toString(), Toast.LENGTH_LONG).show()
+            val sims = googleGroupsRepository.getSims("uss-odyssey-oe", 5)
+            withContext(Main) {
+                sims?.apply {
+                    Toast.makeText(this@MainActivity, sims.toString(), Toast.LENGTH_LONG).show()
+                } ?: run {
+                    Toast.makeText(this@MainActivity, "Could not get sims!", Toast.LENGTH_LONG)
+                        .show()
                 }
             }
-
         }
     }
 }
---
+++ app/src/main/java/com/vandenbreemen/modernsimmingapp/data/repository/GoogleGroupsRepository.kt
@@ -0,0 +1,20 @@
+package com.vandenbreemen.modernsimmingapp.data.repository
+
+import com.vandenbreemen.modernsimmingapp.data.googlegroups.GoogleGroupsAPI
+import com.vandenbreemen.modernsimmingapp.data.googlegroups.GoogleGroupsPost
+
+/**
+ *
+ */
+class GoogleGroupsRepository(private val api: GoogleGroupsAPI) {
+
+    suspend fun getSims(groupName: String, num: Int): List<GoogleGroupsPost>? {
+        val call = api.getRssFeed(groupName, num)
+        val response = call.execute()
+        if(response.isSuccessful) {
+            return response.body()?.articleList
+        }
+        return null
+    }
+
+}

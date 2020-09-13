bc686ca3498cab09405c9b8dcca5fed9daf69ca8
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-09-12 20:15:33 -0400

    Use actual data to fulfil content provider request for group names

--- app/src/main/java/com/vandenbreemen/modernsimmingapp/MainActivity.kt
+++ app/src/main/java/com/vandenbreemen/modernsimmingapp/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.vandenbreemen.modernsimmingapp
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
 import com.vandenbreemen.modernsimmingapp.data.googlegroups.GoogleGroupsAPI
 import com.vandenbreemen.modernsimmingapp.data.googlegroups.GoogleGroupsPost
 import com.vandenbreemen.modernsimmingapp.data.googlegroups.GooglePostContentLoader
 import com.vandenbreemen.modernsimmingapp.data.repository.GoogleGroupsRepository
+import com.vandenbreemen.modernsimmingapp.services.PostFetchingWorker
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Dispatchers.Main
@@ -17,6 +22,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory
+import java.util.concurrent.TimeUnit
 
 const val GOOGLE_GROUPS_BASE_URL = "https://groups.google.com/"
 
@@ -64,9 +70,46 @@ class MainActivity : AppCompatActivity() {
 
     fun doTestContentProvider(view: View) {
 
-        val url = "content://${SimContentProvider::class.java.canonicalName}/groups/hello-world/list"
+        val url = "content://${SimContentProvider::class.java.canonicalName}/groups/"
         Log.i("MAIN", "url=$url")
-        applicationContext.contentResolver.query(Uri.parse(url), emptyArray(),null,null)
 
+        CoroutineScope(Dispatchers.IO).launch {
+            applicationContext.contentResolver.query(Uri.parse(url), emptyArray(),null,null)?.let { cursor ->
+                try {
+
+                    val groupNames: MutableList<String> = mutableListOf()
+
+                    if(cursor.moveToFirst()) {
+                        do {
+                            groupNames.add(cursor.getString(cursor.getColumnIndex("name")))
+                        } while(cursor.moveToNext())
+                    }
+
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@MainActivity, groupNames.toString(), Toast.LENGTH_LONG).show()
+                    }
+
+                } finally {
+                    cursor.close()
+                }
+            }
+
+        }
+
+
+    }
+
+    fun testWorkRequest(view: View) {
+
+        val workRequest = PeriodicWorkRequest.Builder(PostFetchingWorker::class.java, 5, TimeUnit.SECONDS).build()
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork("${applicationContext.packageName}/${PostFetchingWorker::class.java.canonicalName}",
+                ExistingPeriodicWorkPolicy.KEEP, workRequest
+            )
+
+    }
+
+    fun addGroup(view: View) {
+        startActivity(Intent(this, ActivityAddGroup::class.java))
     }
 }
--- app/src/main/java/com/vandenbreemen/modernsimmingapp/SimContentProvider.kt
+++ app/src/main/java/com/vandenbreemen/modernsimmingapp/SimContentProvider.kt
@@ -5,10 +5,21 @@ import android.content.ContentValues
 import android.content.UriMatcher
 import android.content.UriMatcher.NO_MATCH
 import android.database.Cursor
+import android.database.MatrixCursor
 import android.net.Uri
 import android.util.Log
+import com.vandenbreemen.modernsimmingapp.data.localstorage.PostsDatabase
+import com.vandenbreemen.modernsimmingapp.di.SimpleDI
 
 class SimContentProvider : ContentProvider() {
+
+    private val postDatabase: PostsDatabase by lazy {
+        context?.let { c->
+            return@lazy SimpleDI().getPostsDatabase(c)
+        }
+        throw RuntimeException("Context unavailable.  Cannot provide this service!")
+
+    }
 
     companion object {
 
@@ -54,10 +65,18 @@ class SimContentProvider : ContentProvider() {
         if(uriMatcher.match(uri) == ID_LIST_SIMS_IN_GRP) {
 
             Log.i(AUTHORITY, "LIST SIMS IN GROUP")
+            val groupName = uri.pathSegments[1]
             Log.i(AUTHORITY, "grpName=${uri.pathSegments[1]}")
 
+
+
         } else if(uriMatcher.match(uri) == ID_LIST_GROUPS) {
-            Log.i(AUTHORITY, "LIST GROUPS")
+            val groups = postDatabase.groupDao().list()
+            val cursor = MatrixCursor(arrayOf("name"))
+            groups.forEach { group->
+                cursor.newRow().add(group.name)
+            }
+            return cursor
         }
 
         return null

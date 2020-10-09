4de958cf46e9d70fb1f40c0eb46cda3320124573
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-10-08 22:05:12 -0400

    Delegate to simming content subscriber lib so as to keep common code consistent across projects

--- app/src/main/java/com/vandenbreemen/modernsimmingapp/broadcast/Broadcaster.kt
+++ app/src/main/java/com/vandenbreemen/modernsimmingapp/broadcast/Broadcaster.kt
@@ -2,15 +2,16 @@ package com.vandenbreemen.modernsimmingapp.broadcast
 
 import android.content.Context
 import android.content.Intent
+import com.vandenbreemen.modernsimmingcontentsubscriber.ModernSimmingBroadcasting
 
 class Broadcaster(private val context: Context) {
 
     companion object {
         const val NEW_POST_IN_GROUP = "NewPostInGroup"
-        const val PARAM_GROUP_NAME = "groupName"
-        const val PARAM_TTS_CURRENT_POSITION = "position"
-        const val PARAM_TTS_TOTAL_STRINGS_TO_SPEAK = "totalStringsToSpeak"
-        const val TTS_SEEK_TO = "TTSSeekTo"
+        const val PARAM_GROUP_NAME = ModernSimmingBroadcasting.PARAM_GROUP_NAME
+        const val PARAM_TTS_CURRENT_POSITION = ModernSimmingBroadcasting.PARAM_TTS_CURRENT_POSITION
+        const val PARAM_TTS_TOTAL_STRINGS_TO_SPEAK = ModernSimmingBroadcasting.PARAM_TTS_TOTAL_STRINGS_TO_SPEAK
+        const val TTS_SEEK_TO = ModernSimmingBroadcasting.TTS_SEEK_TO
     }
 
     fun sendBroadcastForNewPostInGroup(groupName: String) {

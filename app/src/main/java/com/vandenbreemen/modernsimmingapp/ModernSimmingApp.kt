67a071925086ff630fe6e650ec06c850bf284af6
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-11-14 15:51:54 -0500

    Set up notification channel for the app

--- app/src/main/java/com/vandenbreemen/modernsimmingapp/ModernSimmingApp.kt
+++ app/src/main/java/com/vandenbreemen/modernsimmingapp/ModernSimmingApp.kt
@@ -1,12 +1,27 @@
 package com.vandenbreemen.modernsimmingapp
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class ModernSimmingApp: Application() {
 
+    companion object {
+        const val NOTIFICATION_ID = "NotificationChannel.ModernSimmingApp"
+    }
+
     override fun onCreate() {
         super.onCreate()
+        setupNotificationChannel()
+    }
+
+    fun setupNotificationChannel() {
+        val notificationChannel = NotificationChannel(NOTIFICATION_ID, getString(R.string.notification_name), NotificationManager.IMPORTANCE_DEFAULT)
+        (getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager)?.run {
+            createNotificationChannel(notificationChannel)
+        }
     }
 }
